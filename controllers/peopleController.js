d1a3ff3a1a114b845a4274e53d51f60febcdefe5
mtejedap <mtejeda@purdue.edu>
2023-11-28 14:55:25 -0300

    Add ability to create posts and display them on home page

--- controllers/peopleController.js
+++ controllers/peopleController.js
@@ -1,7 +1,40 @@
 const asyncHandler = require("express-async-handler");
 const User = require('../models/user');
+const Post = require('../models/post');
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
     const user = await User.findOne({ username: req.user.username }).exec();
-    res.render("home", { user: user });
+    console.log(user.id);
+    const posts = await Post.find({ user: user.username }).exec();
+    console.log(posts);
+    res.render("home", { user: user, posts: posts });
 });
+
+exports.post_create_get = asyncHandler(async (req, res, next) => {
+    res.render("post_form");
+});
+
+exports.post_create_post = [
+    body("text", "Text must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        const post = new Post({
+            text: req.body.text,
+            date: new Date(),
+            user: req.user.username
+        });
+        if (!errors.isEmpty()) {
+            res.render("home", {
+                post: post,
+                errors: errors.array()
+            });
+        } else {
+            await post.save();
+            res.redirect(post.url);
+        }
+    })
+];
--- models/post.js
+++ models/post.js
@@ -5,8 +5,12 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
     text: { type: String, required: true },
     date: { type: Date, required: true },
-    user: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    user: { type: String, required: true },
     comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }]
+});
+
+PostSchema.virtual("url").get(function () {
+    return `/people/${this.user}/posts/${this._id}`;
 });
 
 module.exports = mongoose.model("Post", PostSchema);
