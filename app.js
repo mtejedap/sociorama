706ac1571ae1f9d16ad1e49bf55d05f5512db9e0
mtejedap <mtejeda@purdue.edu>
2023-12-07 14:59:50 -0300

    Improve 404 error handling for security purposes

--- app.js
+++ app.js
@@ -41,20 +41,14 @@ const peopleRouter = require('./routes/people');
 app.use('/', loginRouter);
 app.use('/people', peopleRouter);
 
-// catch 404 and forward to error handler
+// Catch illegal route
 app.use(function(req, res, next) {
     next(createError(404));
 });
 
-// error handler
+// Redirect all illegal routes back to root route
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+    res.redirect("/");
 });
 
 module.exports = app;
